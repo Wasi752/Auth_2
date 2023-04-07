178d31298ba5ebf5022b5e308ec5d6cf113b19a0
https://github.com/Wasi752 <osiur2@gmail.com>
2023-04-07 09:30:14 +0600

    Try to Auth

--- cryptography.js
+++ cryptography.js
@@ -7,9 +7,19 @@ const iv = crypto.randomBytes(16);
 //Encrypting text
 function encrypt(text) {
    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
-   let encrypted = cipher.update(text);
+   let encrypted = cipher.update(text + ":" + getCurrentDate());
    encrypted = Buffer.concat([encrypted, cipher.final()]);
-   return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+   return { iv: iv.toString('hex'), code: encrypted.toString('hex') };
+}
+
+// Decrypting text
+function decrypt(text) {
+   let iv = Buffer.from(text.iv, 'hex');
+   let encryptedText = Buffer.from(text.code, 'hex');
+   let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+   let decrypted = decipher.update(encryptedText);
+   decrypted = Buffer.concat([decrypted, decipher.final()]);
+   return decrypted.toString();
 }
 // Get Current Date
 function getCurrentDate() {
@@ -20,20 +30,10 @@ function getCurrentDate() {
 function check(password, code) {
    const decrypted = decrypt(code)
    const [pass, date] = decrypted.split(':')
-   if (pass === password && date === getCurrentDate()) {
+   if (pass === password && parseInt(date) === getCurrentDate()) {
       return true;
    } else {
       return false;
    }
 }
-
-// Decrypting text
-function decrypt(text) {
-   let iv = Buffer.from(text.iv, 'hex');
-   let encryptedText = Buffer.from(text.encryptedData, 'hex');
-   let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
-   let decrypted = decipher.update(encryptedText);
-   decrypted = Buffer.concat([decrypted, decipher.final()]);
-   return decrypted.toString();
-}
-module.exports = { encrypt, decrypt, getCurrentDate }
+module.exports = { encrypt, decrypt, getCurrentDate, check }
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { encrypt, decrypt, getCurrentDate} = require('./cryptography')
+const { encrypt, check } = require('./cryptography')
 const express = require('express')
 const app = express();
 const port = 3001;
 const userID = 77;
+const userName = 'Wasi';
 const userPassword = 'sec1234';
 const encryptedPassword = 'c62f0a413b440463e46a85cc5542e75f';
 
@@ -36,7 +37,7 @@ app.get('/kitab-3', (req, res) => {
         res.send('Unauthorised Entry')
     }
 })
-// Get Encrypted Password
+// Get Encrypted Code
 app.get('/kitab-4', (req, res) => {
     const { id, password } = req.query;
     if (parseInt(id) === userID && password === userPassword) {
@@ -45,10 +46,46 @@ app.get('/kitab-4', (req, res) => {
         res.send(encrypt('77'))
     }
 })
-app.get('/kitab-5', (req, res)=>{
-    const {id, password} = req.query;
-    if (parseInt(id) === userID && password === encryptedPassword){
-        res.send(`Welcome to ${parseInt(id)}`)
+app.get('/kitab-5', (req, res) => {
+    const { id, password } = req.query;
+    if (parseInt(id) === userID && password === encryptedPassword) {
+        res.send(`Welcome to ${userName}`)
+    } else {
+        res.send('You are not Allowed')
+    }
+})
+app.get('/kitab-6', (req, res) => {
+    const { code } = req.query;
+    if (check(userPassword, JSON.parse(code))) {
+        res.send('Secret Message!')
+    } else {
+        res.send('Unauthorised Entry')
+    }
+})
+app.get('/login', (req, res) => {
+    const { password} = req.query;
+    if (password === userPassword) {
+        res.send(encrypt(password))
+    } else {
+        res.send('Unauthorised')
+    }
+})
+// Query ID, Password & Encrypt Password
+app.get('/login-2', (req, res) => {
+    const { id, password } = req.query
+    if (parseInt(id) === userID && password === userPassword) {
+        res.send(encrypt(password))
+    } else {
+        res.send('Unauthorized')
+    }
+})
+
+//Login with Encrypted Password [iv, code]
+app.get('/kitab-7', (req, res) => {
+    const code = req.header('Authorization');
+    const iv = req.header('IV');
+    if (check(userPassword, { code, iv })) {
+        res.send('Secret Message!')
     } else {
         res.send('Unauthorised Entry')
     }
